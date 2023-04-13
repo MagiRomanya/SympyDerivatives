2eeeb8f6113447c03758220e69a756b5b22635ba
Magí Romanyà <magiromanya@gmail.com>
2023-04-13 16:46:20 +0200

    First commit

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    double k, L, L0, x1,x2, y1,y2,z1,z2;
+    double a[3][3] = @PARAMETER;
+    return 0;
+}
