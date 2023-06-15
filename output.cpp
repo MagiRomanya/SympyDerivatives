f0c98d2cbd06682d5f34048981e0bb0f4674585e
Magí Romanyà <magiromanya@gmail.com>
2023-06-15 15:01:34 +0200

    New energy alpha parameter for bending springs

--- output.cpp
+++ output.cpp
@@ -7,6 +7,6 @@ int main()
     double k, L, L0, x1,x2, y1,y2,z1,z2;
     double a[3][3] = 
 
-{{k - L0*k/L + L0*k*pow(x1 - x2, 2)/pow(L, 3), L0*k*(x1 - x2)*(y1 - y2)/pow(L, 3), L0*k*(x1 - x2)*(z1 - z2)/pow(L, 3)}, {L0*k*(x1 - x2)*(y1 - y2)/pow(L, 3), k - L0*k/L + L0*k*pow(y1 - y2, 2)/pow(L, 3), L0*k*(y1 - y2)*(z1 - z2)/pow(L, 3)}, {L0*k*(x1 - x2)*(z1 - z2)/pow(L, 3), L0*k*(y1 - y2)*(z1 - z2)/pow(L, 3), k - L0*k/L + L0*k*pow(z1 - z2, 2)/pow(L, 3)}};
+{{alpha*k - L0*alpha*k/L + L0*alpha*k*pow(x1 - x2, 2)/pow(L, 3), L0*alpha*k*(x1 - x2)*(y1 - y2)/pow(L, 3), L0*alpha*k*(x1 - x2)*(z1 - z2)/pow(L, 3)}, {L0*alpha*k*(x1 - x2)*(y1 - y2)/pow(L, 3), alpha*k - L0*alpha*k/L + L0*alpha*k*pow(y1 - y2, 2)/pow(L, 3), L0*alpha*k*(y1 - y2)*(z1 - z2)/pow(L, 3)}, {L0*alpha*k*(x1 - x2)*(z1 - z2)/pow(L, 3), L0*alpha*k*(y1 - y2)*(z1 - z2)/pow(L, 3), alpha*k - L0*alpha*k/L + L0*alpha*k*pow(z1 - z2, 2)/pow(L, 3)}};
     return 0;
 }
